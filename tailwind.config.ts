d319af04e055668b9355b94f26fae45e83ccd20c
fokx <36614441+fokx@users.noreply.github.com>
2024-12-26 22:48:25 +0800

    adjust css to lightbox and onebox cooked by discourse

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
@@ -14,8 +13,38 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B' },
-				secondary: { "50": "#f0f9ff", "100": "#e0f2fe", "200": "#bae6fd", "300": "#7dd3fc", "400": "#38bdf8", "500": "#0ea5e9", "600": "#0284c7", "700": "#0369a1", "800": "#075985", "900": "#0c4a6e" },
+				primary: {
+					50: '#FFF5F2',
+					100: '#FFF1EE',
+					200: '#FFE4DE',
+					300: '#FFD5CC',
+					400: '#FFBCAD',
+					500: '#FE795D',
+					600: '#EF562F',
+					700: '#EB4F27',
+					800: '#CC4522',
+					900: '#A5371B',
+					925: '#883018',
+					950: '#6d2816',
+					975: '#522113',
+					1000: '#39190f'
+				},
+				secondary: {
+					50: '#f0f9ff',
+					100: '#e0f2fe',
+					200: '#bae6fd',
+					300: '#7dd3fc',
+					400: '#38bdf8',
+					500: '#0ea5e9',
+					600: '#0284c7',
+					700: '#0369a1',
+					800: '#075985',
+					900: '#0c4a6e',
+					925: '#113e5b',
+					950: '#133249',
+					975: '#132737',
+					1000: '#111c27'
+				}
 			}
 		}
 	},

57d5b5255588f4a7d0715f472f3589c6f285ec29
fokx <36614441+fokx@users.noreply.github.com>
2025-01-17 19:14:28 +0800

    use static build; add basic tauri files from tauri-app template; change dev server port from 5173 to 1420

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { resolve } from 'path';
 
-export default defineConfig({
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig(async () => ({
 	plugins: [sveltekit()],
 
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
@@ -12,6 +19,22 @@ export default defineConfig({
 		alias: {
 			$images: resolve('./static/images/')
 		}
+	},
+	server: {
+		port: 1420,
+		strictPort: true,
+		host: host || false,
+		hmr: host
+			? {
+				protocol: "ws",
+				host,
+				port: 1421,
+			}
+			: undefined,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ["**/src-tauri/**"],
+		},
 	}
 	//
 	// server: {
@@ -23,4 +46,4 @@ export default defineConfig({
 	// 		},
 	// 	},
 	// },
-})
+}));
